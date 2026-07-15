1e134f25dd0e7164ad8bf4da1a9618bf1be77ff4
yeye <yeyeshmg@gmail.com>
2026-07-15 13:35:07 +0900

    v6-darkmd-etc

--- src/constants/storage.js
+++ src/constants/storage.js
@@ -1,4 +1,5 @@
 export const POST_STORAGE_KEY = 'localhub.posts.v1'
+export const THEME_STORAGE_KEY = 'localhub.theme'
 
 export const POST_CATEGORY_OPTIONS = Object.freeze([
   { value: 'free', label: '자유' },
